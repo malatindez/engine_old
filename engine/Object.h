764c207166c3abe612e0965f675c7965596f5a8d
malatindez <axenance@gmail.com>
2020-06-23 23:35:06 +0300

    Update Object.h

--- engine/Object.h
+++ engine/Object.h
@@ -7,6 +7,7 @@
 
 #include "Model.h"
 class Object : public Model {
+protected:
 	glm::mat4 translationMatrix = glm::mat4(1.0f);
 	glm::mat4 rotationMatrix = glm::mat4(1.0f);
 	glm::mat4 scaleMatrix = glm::mat4(1.0f);
@@ -21,14 +22,20 @@ class Object : public Model {
 public:
 	// angle should be defined in radians
 	Object(std::string path, TextureManager* const mngr, 
-		glm::vec3 coords = glm::vec3(0.0f), glm::vec3 angle = glm::vec3(0.0f),
+		glm::vec3 coords = glm::vec3(0.0f), glm::vec3 angle = glm::vec3(3.1415927f),
 		glm::vec3 scale = glm::vec3(1.0f)
 	) : Model(path, mngr) {
 		this->move(coords);
 		this->rotate(angle);
 		this->scale(scale);
 	}
-
+	Object(std::vector<std::shared_ptr<Mesh>> meshes, TextureManager* const mngr,
+		glm::vec3 coords = glm::vec3(0.0f), glm::vec3 angle = glm::vec3(0.0f),
+		glm::vec3 scale = glm::vec3(1.0f)) : Model(meshes, mngr) {
+		this->move(coords);
+		this->rotate(angle);
+		this->scale(scale);
+	}
 	// This function returns rotation & coordinate matrix, which we can process & use in shader
 	glm::mat4 getObjectMatrix() noexcept;
 
@@ -48,6 +55,11 @@ public:
 	void scaleX(const int scale) noexcept;
 	void scaleY(const int scale) noexcept;
 	void scaleZ(const int scale) noexcept;
+
+	virtual void Draw(std::shared_ptr<ShaderProgram> program) noexcept {
+		program->setMat4("model", getObjectMatrix());
+		Model::Draw(program);
+	}
 };
 
 #endif
