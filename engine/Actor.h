2e4c7954d624444c174c1726fc67c228c77ef6c5
malatindez <axenance@gmail.com>
2020-06-23 23:34:43 +0300

    Update Actor.h

--- engine/Actor.h
+++ engine/Actor.h
@@ -3,11 +3,23 @@
 template<int tickrate>
 class Actor : public Object, public Ticker<tickrate> {
 public:
-	virtual void Update(const unsigned int tick) const noexcept {
-
+	unsigned int getTickRate() const noexcept {
+		return tickrate;
 	}
+	virtual void Update(const unsigned int tick, const float deltaTime) noexcept {
+		if (tick % getTickRate() == 0) {
+			this->rotateY(360.0f * deltaTime);
+		}
+	}
+	glm::vec3 velocity = glm::vec3(0.0f), acceleration = glm::vec3(0.0f, 0.01f, 0.0f);
+	void Move(const float deltaTime) noexcept {
+			float m = deltaTime * deltaTime;
+			this->move(this->velocity * deltaTime + acceleration * m);
+			this->velocity = this->velocity + acceleration * m;
+	}
+	
 	Actor(std::string path, TextureManager* const mngr,
-		glm::vec3 coords = glm::vec3(1.0f), glm::vec3 angle = glm::vec3(1.0f),
+		glm::vec3 coords = glm::vec3(0.0f), glm::vec3 angle = glm::vec3(3.1415927f),
 		glm::vec3 scale = glm::vec3(1.0f)) : Object(path, mngr, coords, angle, scale) {
 
 	}
