60518b1c1f84dcebc9916dc33f2e9cfb4f776fbd
malatindez <axenance@gmail.com>
2020-06-24 13:08:23 +0300

    Update Mesh.h

--- engine/Mesh.h
+++ engine/Mesh.h
@@ -5,9 +5,8 @@
 class Mesh {
 public:
     struct Vertex {
-        glm::vec3 Position;
-        glm::vec3 Normal;
-        glm::vec2 TexCoords;
+        glm::vec3 position;
+        glm::vec2 texCoords;
     };
 private:
     //  render data
@@ -20,7 +19,6 @@ private:
 
         glBindVertexArray(VAO);
         glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
         glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(Vertex), &vertices[0], GL_STATIC_DRAW);
 
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
@@ -30,27 +28,34 @@ private:
         // vertex positions
         glEnableVertexAttribArray(0);
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
-        // vertex normals
+
         glEnableVertexAttribArray(1);
-        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, Normal));
-        // vertex texture coords
-        glEnableVertexAttribArray(2);
-        glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, TexCoords));
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex,texCoords));
 
         glBindVertexArray(0);
     }
-    // copy constructor
-    Mesh(const Mesh& other) noexcept { }
 
     // move constructor
     Mesh(Mesh&& other) noexcept {  }
 
-    // copy assignment
-    Mesh& operator=(const Mesh& other) noexcept { }
 
     // move assignment
     Mesh& operator=(Mesh&& other) noexcept { }
 public:
+    // copy constructor
+    Mesh(const Mesh& other) noexcept {
+        this->vertices = other.vertices;
+        this->indices = other.indices;
+        this->textures = other.textures;
+        setupMesh();
+    }
+    // copy assignment
+    Mesh& operator=(const Mesh& other) noexcept {
+        this->vertices = other.vertices;
+        this->indices = other.indices;
+        this->textures = other.textures;
+        setupMesh();
+    }
     // mesh data
     std::vector<Vertex>       vertices;
     std::vector<unsigned int> indices;
@@ -64,23 +69,29 @@ public:
         setupMesh();
     }
     ~Mesh() {
-        glGenBuffers(1, &EBO);
-        glGenBuffers(1, &VBO);
+        glDeleteBuffers(1, &EBO);
+        glDeleteBuffers(1, &VBO);
         glDeleteVertexArrays(1, &VAO);
     }
 
     void Draw(std::shared_ptr<ShaderProgram> shader) const noexcept {
-        unsigned int diffuseNr = 1;
-        unsigned int specularNr = 1;
+        unsigned int diffuseNr = 0;
+        unsigned int specularNr = 0;
         for (unsigned int i = 0; i < textures.size(); i++) {
             glActiveTexture(GL_TEXTURE0 + i); // activate proper texture unit before binding
             // retrieve texture number (the N in diffuse_textureN)
             std::string number;
             if (textures[i]->type == Texture::Type::DIFFUSE) {
-                shader->setInt(("material.diffuse[" + std::to_string(diffuseNr++) + "]").c_str(), i);
+                shader->setInt(("material.diffuseTextures[" + std::to_string(diffuseNr++) + "]"), i);
             }
             else if (textures[i]->type == Texture::Type::SPECULAR) {
-                shader->setInt(("material.specular[" + std::to_string(specularNr++) + "]").c_str(), i);
+                shader->setInt(("material.specularTextures[" + std::to_string(specularNr++) + "]"), i);
+            }
+            else if (textures[i]->type == Texture::Type::NORMALS) {
+                shader->setInt("normal", i);
+            }
+            else if (textures[i]->type == Texture::Type::HEIGHT) {
+                shader->setInt("material.diffuseTextures[" + std::to_string(diffuseNr++) + "]", i);
             }
             glBindTexture(GL_TEXTURE_2D, textures[i]->getID());
         }
