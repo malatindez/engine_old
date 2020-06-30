f19b1900f969b8df82928fc12ab74fe8237e477f
malatindez <axenance@gmail.com>
2020-06-30 20:49:36 +0300

    Update Object.h

--- engine/Object.h
+++ engine/Object.h
@@ -29,6 +29,7 @@ public:
 		this->rotate(angle);
 		this->scale(scale);
 	}
+	// angle should be defined in radians
 	Object(std::vector<std::shared_ptr<Mesh>> meshes, TextureManager* const mngr,
 		glm::vec3 coords = glm::vec3(0.0f), glm::vec3 angle = glm::vec3(0.0f),
 		glm::vec3 scale = glm::vec3(1.0f)) : Model(meshes, mngr) {
@@ -39,30 +40,53 @@ public:
 	// This function returns rotation & coordinate matrix, which we can process & use in shader
 	glm::mat4 getObjectMatrix() noexcept;
 
+	//move object by this coords(object.x += coords.x, object.y += coords.y etc.)
 	void move(const glm::vec3 coords) noexcept;
+	//move object by this coords(object.x += coords.x, object.y += coords.y etc.)
 	void move(const float x, const float y, const float z) noexcept;
+	// set translation matrix
 	void setTranslationMatrix(const glm::mat4 mat) noexcept;
+	// set current position
 	void setPosition(const glm::vec3 pos) noexcept;
 
 	// angle should be defined in radians
+	// rotate object by given angle
 	void rotate(const float anglex, const float angley, const float anglez) noexcept;
+	// angle should be defined in radians
+	// rotate object by given angle
 	void rotate(const glm::vec3 angle) noexcept;
+	// angle should be defined in radians
+	// rotate object by given angle
 	void rotateX(const float angle) noexcept;
+	// angle should be defined in radians
+	// rotate object by given angle
 	void rotateY(const float angle) noexcept;
+	// angle should be defined in radians
+	// rotate object by given angle
 	void rotateZ(const float angle) noexcept;
+	// set rotation matrix
 	void setRotationMatrix(const glm::mat4 mat) noexcept;
+	// set rotation angles
 	void setRotation(const glm::vec3 angle) noexcept;
+	// set rotation angles
+	void setRotation(const float anglex, const float angley, const float anglez) noexcept;
 
-
+	// scale object by value (transforms current scale)
 	void scale(const glm::vec3 scale) noexcept;
+	// scale object by value (transforms current scale)
 	void scale(const int x, const int y, const int z) noexcept;
+	// scale object by value (transforms current scale)
 	void scaleX(const int scale) noexcept;
+	// scale object by value (transforms current scale)
 	void scaleY(const int scale) noexcept;
+	// scale object by value (transforms current scale)
 	void scaleZ(const int scale) noexcept;
+	// set scale matrix
 	void setScaleMatrix(const glm::mat4 mat) noexcept;
+	// set current scale
 	void setScale(const glm::vec3 scale) noexcept;
 
-	virtual void Draw(std::shared_ptr<ShaderProgram> program) noexcept {
+	virtual void Draw(std::shared_ptr<ShaderProgram> program, glm::mat4 matrix=glm::mat4(1.0f)) noexcept {
 		program->setMat4("model", getObjectMatrix());
 		Model::Draw(program);
 	}
