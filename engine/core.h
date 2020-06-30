e35dd5c786fbcedf29e119c856bbd510c8fbb7fc
malatindez <axenance@gmail.com>
2020-06-30 20:51:07 +0300

    Update core.h

--- engine/core.h
+++ engine/core.h
@@ -1,7 +1,22 @@
 #ifndef CORE_H
 #define CORE_H
-#include "ShaderWorker.h"
-#include "render.h"
-#include "Texture.h"
-#include "Object.h"
+#include "Ticker.h"
+class GameCore {
+public:
+	unsigned int current_tick = 0;
+	std::vector<std::shared_ptr<Ticker>> tickingObjects;
+	GameCore() {
+		
+	}
+
+	unsigned int getCurrentGlobalTick() const noexcept {
+		return current_tick;
+	}
+	
+	bool appendTickingObject(std::shared_ptr<Ticker> object) {
+		//this->models.push_back(model);
+	}
+
+};
+
 #endif
