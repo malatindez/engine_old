bd273f9a1c3980b5b2087b385b1005e536d4d5c0
malatindez <axenance@gmail.com>
2020-06-30 20:49:46 +0300

    Update Ticker.h

--- engine/Ticker.h
+++ engine/Ticker.h
@@ -1,17 +1,14 @@
 #ifndef TICKER_H
 #define TICKER_H
 
-template<int tickrate>
 class Ticker {
 public:
+	unsigned int tickrate;
+	Ticker(unsigned int tickrate) { this->tickrate = tickrate; }
 	unsigned int getTickRate() const {
-		return tickrate; 
+		return tickrate;
 	}
 	// Parameter of this function is a current global tick(from a game start)
-	void Update(const unsigned int tick, const float delta) noexcept {
-		
-	}
+	void Update(const unsigned int tick, const float delta) noexcept { }
 };
-
-
 #endif
