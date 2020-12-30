147c068d9f083157730a335522cf3762122d90ed
malatindez <axenance@gmail.com>
2020-12-30 19:04:34 +0200

    Update core

---
+++ engine/core/Core.cpp
@@ -0,0 +1,13 @@
+#include "Core.h"
+namespace engine::core {
+
+std::shared_ptr<Core> Core::core_ptr_ = std::shared_ptr<Core>(nullptr);
+
+std::shared_ptr<Core> Core::GetInstance() noexcept {
+  if (core_ptr_ == nullptr) {
+    core_ptr_ = std::shared_ptr<Core>(new Core());
+  }
+  return core_ptr_;
+}
+
+}
--- engine/core/Core.h
+++ engine/core/Core.h
@@ -1,18 +1,34 @@
-#ifndef CORE_H
-#define CORE_H
+#pragma once
 #include <memory>
 #include <vector>
 
 #include "../render/render.h"
 #include "Ticker.h"
+
 namespace engine::core {
+
+// Singleton class instance of which you can get by calling the GetInstance
+// function.
 class Core {
  public:
-  unsigned int current_tick = 0;
-  std::vector<std::shared_ptr<Ticker>> tickingObjects;
-  Core() {}
+
+  /* Disable copy and move semantics. */
+  Core(const Core&) = delete;
+  Core(Core&&) = delete;
+  Core& operator=(const Core&) = delete;
+  Core& operator=(Core&&) = delete;
 
   unsigned int GetCurrentGlobalTick() const noexcept { return current_tick; }
+
+  // returns reference to the Core.
+  static std::shared_ptr<Core> GetInstance() noexcept;
+
+ private:
+  Core() = default;
+  static std::shared_ptr<Core> core_ptr_;
+  
+  unsigned int current_tick = 0;
+
+  std::vector<std::shared_ptr<Ticker>> tickingObjects;
 };
 }  // namespace engine::core
-#endif
