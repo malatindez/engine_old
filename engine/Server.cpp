814ca058f00905f0735c16a90b4f71e4c20be068
malatindez <axenance@gmail.com>
2021-01-08 06:04:37 +0200

    Update Server.cpp

--- engine/Server.cpp
+++ engine/Server.cpp
@@ -1,11 +1,15 @@
 #include "Config.h"
 
 #include <functional>
-#include <iostream>
 
 #include "core/Core.h"
 
+#ifdef WIN32
+#include <Windows.h>
+int WINAPI wWinMain(HINSTANCE, HINSTANCE, PWSTR pCmdLine, int) {
+#else
 int main() {
+#endif
 	
 	return 0;
 }
