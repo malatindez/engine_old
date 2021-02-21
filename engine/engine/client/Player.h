53875c020de26d8dea54f59cb0e71ebd6e027cd4
malatindez <axenance@gmail.com>
2021-02-21 21:46:49 +0200

    Update Player.h

--- engine/engine/client/Player.h
+++ engine/engine/client/Player.h
@@ -4,7 +4,7 @@
 
 #include "misc/Input.h"
 
-namespace engine::client::client {
+namespace engine::client {
 class Player {
  public:
   /* Disable copy and move semantics. */
@@ -13,22 +13,61 @@ class Player {
   Player& operator=(const Player&) = delete;
   Player& operator=(Player&&) = delete;
 
-  Player(std::shared_ptr<Window> window, glm::vec3 worldUp,
-         glm::vec3 position = glm::vec3(0.0F))
-      : position_(position) {
-    this->camera_ = std::make_shared<render::Camera>(
-        render::Camera(window, worldUp, 0, 45.0F));
+  Player(std::shared_ptr<Window> window, glm::vec3 world_up,
+         glm::vec3 position = glm::vec3(0.0F)) {
+    this->camera_ =
+        std::make_shared<render::Camera>(window, world_up, 0, 45.0F);
+    camera_->SetPosition(position);
+    using namespace std::placeholders;
+
+    this->move_cb_ptr_ = std::make_shared<Window::KeyBindCallback>(
+        std::bind(&Player::Move, this, _1, _2));
+
+    window->AddKeyCallback(glfwGetKeyScancode(GLFW_KEY_W), move_cb_ptr_);
+    window->AddKeyCallback(glfwGetKeyScancode(GLFW_KEY_A), move_cb_ptr_);
+    window->AddKeyCallback(glfwGetKeyScancode(GLFW_KEY_S), move_cb_ptr_);
+    window->AddKeyCallback(glfwGetKeyScancode(GLFW_KEY_D), move_cb_ptr_);
   }
 
-  void SetPosition(glm::vec3 x) noexcept { this->position_ = x; }
-  [[nodiscard]] glm::vec3 position() const noexcept { return position_; }
+  void SetPosition(glm::vec3 x) noexcept { camera_->SetPosition(x); }
+  [[nodiscard]] glm::vec3 position() const noexcept { return camera_->position(); }
+
   void SetMovementSpeed(float x) noexcept { this->speed_ = x; }
   [[nodiscard]] float movement_speed() const noexcept { return speed_; }
 
+  [[nodiscard]] std::shared_ptr<render::Camera> camera() const { return camera_; }
+
  private:
+   
+  bool Move(int32_t scancode, int32_t action) {
+    if (action == 0) {
+      return false;
+    }
+    glm::vec3 position = camera_->position();
+    if (scancode == kScancodeW) {
+      position += speed_ * camera_->front();
+    } else if (scancode == kScancodeS) {
+      position -= speed_ * camera_->front();
+    } else if (scancode == kScancodeA) {
+      position -=
+          glm::normalize(glm::cross(camera_->front(), camera_->up())) * speed_;
+    } else if (scancode == kScancodeD) {
+      position +=
+          glm::normalize(glm::cross(camera_->front(), camera_->up())) * speed_;
+    }
+    camera_->SetPosition(position);
+    return false; 
+  }
+
+  const int32_t kScancodeW = glfwGetKeyScancode(GLFW_KEY_W);
+  const int32_t kScancodeA = glfwGetKeyScancode(GLFW_KEY_A);
+  const int32_t kScancodeS = glfwGetKeyScancode(GLFW_KEY_S);
+  const int32_t kScancodeD = glfwGetKeyScancode(GLFW_KEY_D);
+
   std::shared_ptr<render::Camera> camera_;
 
-  glm::vec3 position_;
-  float speed_ = 1.0F;
+  float speed_ = 0.05F;
+
+  std::shared_ptr<Window::KeyBindCallback> move_cb_ptr_;
 };
 }  // namespace engine::client::client
