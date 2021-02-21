6888a007d638ffa8fe3385d49a44c84cbe9367ad
malatindez <axenance@gmail.com>
2021-02-21 21:46:57 +0200

    Create Renderer.h

---
+++ engine/engine/client/render/Renderer.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <memory>
+
+#include "Shader.h"
+#include "engine/Object.h"
+
+namespace engine::core {
+class Object;
+}
+namespace engine::client::render {
+class Renderer {
+ public:
+  virtual ~Renderer() = default;
+
+  // Should return a shader program which will be applied before Draw() call
+  // Multiple objects can have the same shader, so the Render engine will
+  // optimize shader calls
+  virtual std::weak_ptr<Shader> shader() const noexcept { return {}; }
+
+  virtual void Draw(std::weak_ptr<engine::core::Object> object) {
+    // intentionally unimplemented
+  }
+};
+}  // namespace engine::client::render
