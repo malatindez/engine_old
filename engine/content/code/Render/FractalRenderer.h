cdf60842a8b92e0dd055e8a8b2a7eae2043694c9
malatindez <axenance@gmail.com>
2021-02-23 01:24:06 +0200

    Update FractalRenderer.h

--- engine/content/code/Render/FractalRenderer.h
+++ engine/content/code/Render/FractalRenderer.h
@@ -9,22 +9,20 @@ class FractalRenderer : public engine::client::render::Renderer {
   FractalRenderer() {
     using engine::client::render::Mesh;
     std::vector<Mesh::Vertex> vertices = {
-        Mesh::Vertex(glm::vec3(-1, -1, -1), glm::vec2(0, 0)),
-        Mesh::Vertex(glm::vec3(1, -1, -1), glm::vec2(1, 0)),
-        Mesh::Vertex(glm::vec3(1, 1, -1), glm::vec2(1, 1)),
-        Mesh::Vertex(glm::vec3(-1, 1, -1), glm::vec2(0, 1)),
-        Mesh::Vertex(glm::vec3(-1, -1, 1), glm::vec2(0, 0)),
-        Mesh::Vertex(glm::vec3(1, -1, 1), glm::vec2(1, 0)),
-        Mesh::Vertex(glm::vec3(1, 1, 1), glm::vec2(1, 1)),
-        Mesh::Vertex(glm::vec3(-1, 1, 1), glm::vec2(0, 1)),
+        Mesh::Vertex(glm::vec3(0.5, 0.5, 0), glm::vec2(1, 1)),
+        Mesh::Vertex(glm::vec3(0.5, -0.5, 0), glm::vec2(1, 0)),
+        Mesh::Vertex(glm::vec3(-0.5, -0.5, 0), glm::vec2(0, 0)),
+        Mesh::Vertex(glm::vec3(-0.5, 0.5, 0), glm::vec2(0, 1)),
     };
-    std::vector<unsigned int> indices = {0, 1, 3, 3, 1, 2, 1, 5, 2, 2, 5, 6,
-                                         5, 4, 6, 6, 4, 7, 4, 0, 7, 7, 0, 3,
-                                         3, 2, 7, 7, 2, 6, 4, 5, 0, 0, 5, 1};
-    auto vertices_ptr = std::make_shared<std::vector<Mesh::Vertex>>(vertices);
-    auto indices_ptr = std::make_shared<std::vector<unsigned int>>(indices);
-    mesh_ = std::make_shared<Mesh>(vertices_ptr, indices_ptr);
-    fractal_shader_ = engine::core::Core::GetInstance()->LoadShader("engine\\content\\shaders\\triangle.vert", "engine\\content\\shaders\\triangle.frag");
+    std::vector<unsigned int> indices = {0, 1, 3, 1,2,3};
+    vertices_ = std::make_shared<std::vector<Mesh::Vertex>>();
+    indices_ = std::make_shared<std::vector<unsigned int>>();
+    vertices_->assign(vertices.begin(), vertices.end());
+    indices_->assign(indices.begin(), indices.end());
+    mesh_ = std::make_shared<Mesh>(vertices_, indices_);
+    fractal_shader_ = engine::core::Core::GetInstance()->LoadShader(
+        "F:\\cpp\\MVS\\engine\\engine\\content\\shaders\\triangle.vert",
+        "F:\\cpp\\MVS\\engine\\engine\\content\\shaders\\triangle.frag");
   }
   std::weak_ptr<engine::client::render::Shader> shader()
       const noexcept override {
@@ -39,5 +37,8 @@ class FractalRenderer : public engine::client::render::Renderer {
  private:
   std::shared_ptr<engine::client::render::Mesh> mesh_;
   std::shared_ptr<engine::client::render::Shader> fractal_shader_;
+
+  std::shared_ptr<std::vector<engine::client::render::Mesh::Vertex>> vertices_;
+  std::shared_ptr<std::vector<unsigned int>> indices_;
 };
 }  // namespace content::render
