5d3baa00aa6a1c93645bf69e5369cd93a99bffa2
malatindez <axenance@gmail.com>
2021-03-08 10:58:05 +0200

    Reduce Shader::Shader complexity and add shader hashing function

--- engine/engine/client/render/Shader.cpp
+++ engine/engine/client/render/Shader.cpp
@@ -1,51 +1,57 @@
 #include "Shader.h"
 namespace engine::client::render {
 
-//
-int32_t Shader::CompileShader(std::string_view shader_code, uint32_t& id, GLenum type) {
+int32_t Shader::CompileShader(std::string_view shader_code, uint32_t& id,
+                              GLenum type) {
   id = glCreateShader(type);
   const char* shader_code_c_str = shader_code.data();
   glShaderSource(id, 1, &shader_code_c_str, nullptr);
   glCompileShader(id);
-  int32_t success;
-  glGetShaderiv(id, GL_COMPILE_STATUS, &success);
+  int32_t success = 0;
+  if (!success) {
+    glGetShaderiv(id, GL_COMPILE_STATUS, &success);
+    GLchar info_log[1024];
+    glGetShaderInfoLog(id, 1024, nullptr, info_log);
+    std::cout << info_log << std::endl;
+    // TODO exception output
+  }
   return success;
 }
+std::size_t Shader::ShaderSource::hash() {
+  return std::hash<ShaderSource>()(*this);
+}
 
-Shader::Shader(std::string_view vertex_shader_code,
-               std::string_view fragment_shader_code,
-               std::string_view geometry_shader_code) {
-  uint32_t vertex   = -1;
+Shader::Shader(ShaderSource const& source) {
+  uint32_t vertex = -1;
   uint32_t fragment = -1;
   uint32_t geometry = -1;
 
-
-  if (!CompileShader(vertex_shader_code, vertex, GL_VERTEX_SHADER)) {
+  if (!CompileShader(source.vertex_shader_code, vertex, GL_VERTEX_SHADER)) {
     glDeleteShader(vertex);
-    // TODO exception output
     return;
   }
 
-  if (!CompileShader(fragment_shader_code, fragment, GL_FRAGMENT_SHADER)) {
-    glDeleteShader(fragment);
+  if (!CompileShader(source.fragment_shader_code, fragment,
+                     GL_FRAGMENT_SHADER)) {
     glDeleteShader(vertex);
-    // TODO exception output
+    glDeleteShader(fragment);
     return;
   }
 
-  if (geometry_shader_code != "" &&
-      !CompileShader(geometry_shader_code, geometry, GL_GEOMETRY_SHADER)) {
-    glDeleteShader(fragment);
+  if (source.geometry_shader_code != "" &&
+      !CompileShader(source.geometry_shader_code, geometry,
+                     GL_GEOMETRY_SHADER)) {
     glDeleteShader(vertex);
+    glDeleteShader(fragment);
     glDeleteShader(geometry);
-    // TODO exception output
     return;
   }
+
   // shader Program
   sp_id_ = glCreateProgram();
   glAttachShader(sp_id_, vertex);
   glAttachShader(sp_id_, fragment);
-  if (geometry_shader_code != "") glAttachShader(sp_id_, geometry);
+  if (source.geometry_shader_code != "") glAttachShader(sp_id_, geometry);
   glLinkProgram(sp_id_);
 
   int32_t compile_success = 0;
@@ -55,17 +61,18 @@ Shader::Shader(std::string_view vertex_shader_code,
     // TODO exception output
     GLchar info_log[1024];
     glGetProgramInfoLog(sp_id_, 1024, nullptr, info_log);
+    std::cout << info_log << std::endl;
 
     glDeleteShader(vertex);
     glDeleteShader(fragment);
-    if (geometry_shader_code != "") glDeleteShader(geometry);
+    if (source.geometry_shader_code != "") glDeleteShader(geometry);
     return;
   }
   // delete the shaders as they're linked into our program now and no longer
-  // necessery
+  // necessary
   glDeleteShader(vertex);
   glDeleteShader(fragment);
-  if (geometry_shader_code != "") glDeleteShader(geometry);
+  if (source.geometry_shader_code != "") glDeleteShader(geometry);
 }
 
 Shader::~Shader() { glDeleteProgram(sp_id_); }
@@ -86,97 +93,90 @@ void Shader::SetFloat(const std::string& name, float value) const {
   glUniform1f(glGetUniformLocation(sp_id_, name.c_str()), value);
 }
 
-void Shader::SetVec1(const std::string& name,
-                            const glm::vec1& value) const {
+void Shader::SetVec1(const std::string& name, const glm::vec1& value) const {
   GLint location = glGetUniformLocation(sp_id_, name.c_str());
   glUniform1fv(location, 1, &value[0]);
 }
 
-void Shader::SetVec2(const std::string& name,
-                            const glm::vec2& value) const {
+void Shader::SetVec2(const std::string& name, const glm::vec2& value) const {
   GLint location = glGetUniformLocation(sp_id_, name.c_str());
   glUniform2fv(location, 1, &value[0]);
 }
 
-void Shader::SetVec3(const std::string& name,
-                            const glm::vec3& value) const {
+void Shader::SetVec3(const std::string& name, const glm::vec3& value) const {
   GLint location = glGetUniformLocation(sp_id_, name.c_str());
   glUniform3fv(location, 1, &value[0]);
 }
 
-void Shader::SetVec4(const std::string& name,
-                            const glm::vec4& value) const {
+void Shader::SetVec4(const std::string& name, const glm::vec4& value) const {
   GLint location = glGetUniformLocation(sp_id_, name.c_str());
   glUniform4fv(location, 1, &value[0]);
 }
 
-void Shader::SetMat2(const std::string& name,
-                            const glm::mat2& value) const {
+void Shader::SetMat2(const std::string& name, const glm::mat2& value) const {
   glUniformMatrix2fv(glGetUniformLocation(sp_id_, name.c_str()), 1, GL_FALSE,
                      &value[0][0]);
 }
 
-void Shader::SetMat3(const std::string& name,
-                            const glm::mat3& value) const {
+void Shader::SetMat3(const std::string& name, const glm::mat3& value) const {
   glUniformMatrix3fv(glGetUniformLocation(sp_id_, name.c_str()), 1, GL_FALSE,
                      &value[0][0]);
 }
 
-void Shader::SetMat4(const std::string& name,
-                            const glm::mat4& value) const {
+void Shader::SetMat4(const std::string& name, const glm::mat4& value) const {
   glUniformMatrix4fv(glGetUniformLocation(sp_id_, name.c_str()), 1, GL_FALSE,
                      &value[0][0]);
 }
 
 void Shader::SetMat2x2(const std::string& name,
-                              const glm::mat2x2& value) const {
+                       const glm::mat2x2& value) const {
   SetMat2(name, value);
 }
 
 void Shader::SetMat2x3(const std::string& name,
-                              const glm::mat2x3& value) const {
+                       const glm::mat2x3& value) const {
   glUniformMatrix2x3fv(glGetUniformLocation(sp_id_, name.c_str()), 1, GL_FALSE,
                        &value[0][0]);
 }
 
 void Shader::SetMat2x4(const std::string& name,
-                              const glm::mat2x4& value) const {
+                       const glm::mat2x4& value) const {
   glUniformMatrix2x4fv(glGetUniformLocation(sp_id_, name.c_str()), 1, GL_FALSE,
                        &value[0][0]);
 }
 
 void Shader::SetMat3x2(const std::string& name,
-                              const glm::mat3x2& value) const {
+                       const glm::mat3x2& value) const {
   glUniformMatrix3x2fv(glGetUniformLocation(sp_id_, name.c_str()), 1, GL_FALSE,
                        &value[0][0]);
 }
 
 void Shader::SetMat3x3(const std::string& name,
-                              const glm::mat3x3& value) const {
+                       const glm::mat3x3& value) const {
   SetMat3(name, value);
 }
 
 void Shader::SetMat3x4(const std::string& name,
-                              const glm::mat3x4& value) const {
+                       const glm::mat3x4& value) const {
   glUniformMatrix2x4fv(glGetUniformLocation(sp_id_, name.c_str()), 1, GL_FALSE,
                        &value[0][0]);
 }
 
 void Shader::SetMat4x2(const std::string& name,
-                              const glm::mat4x2& value) const {
+                       const glm::mat4x2& value) const {
   glUniformMatrix4x2fv(glGetUniformLocation(sp_id_, name.c_str()), 1, GL_FALSE,
                        &value[0][0]);
 }
 
 void Shader::SetMat4x3(const std::string& name,
-                              const glm::mat4x3& value) const {
+                       const glm::mat4x3& value) const {
   glUniformMatrix4x2fv(glGetUniformLocation(sp_id_, name.c_str()), 1, GL_FALSE,
                        &value[0][0]);
 }
 
 void Shader::SetMat4x4(const std::string& name,
-                              const glm::mat4x4& value) const {
+                       const glm::mat4x4& value) const {
   glUniformMatrix4fv(glGetUniformLocation(sp_id_, name.c_str()), 1, GL_FALSE,
                      &value[0][0]);
 }
-}
+}  // namespace engine::client::render
--- engine/engine/client/render/Shader.h
+++ engine/engine/client/render/Shader.h
@@ -1,82 +1,105 @@
 #pragma once
-#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glad/glad.h>
 
+#include <fstream>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
-
-#include <string>
-#include <fstream>
+#include <iostream>
 #include <sstream>
-
+#include <string>
 namespace engine::client::render {
 class Shader {
-   public:
+ public:
+  struct ShaderSource {
+    std::string_view vertex_shader_code;
+    std::string_view fragment_shader_code;
+    std::string_view geometry_shader_code;
+    ShaderSource(std::string_view vertex_shader_code,
+                 std::string_view fragment_shader_code,
+                 std::string_view geometry_shader_code = "")
+        : vertex_shader_code(vertex_shader_code),
+          fragment_shader_code(fragment_shader_code),
+          geometry_shader_code(geometry_shader_code) {}
 
-    /* Disable copy and move semantics. */
-    Shader(const Shader&) = delete;
-    Shader(Shader&&) = delete;
-    Shader& operator=(const Shader&) = delete;
-    Shader& operator=(Shader&&) = delete;
+    [[nodiscard]] std::size_t hash();
+  };
+  /* Disable copy and move semantics. */
+  Shader(const Shader&) = delete;
+  Shader(Shader&&) = delete;
+  Shader& operator=(const Shader&) = delete;
+  Shader& operator=(Shader&&) = delete;
 
-    Shader(std::string_view vertex_shader_code,
-           std::string_view fragment_shader_code,
-           std::string_view geometry_shader_code = "");
+  Shader(ShaderSource const&);
 
-    ~Shader();
+  ~Shader();
 
-    void operator()() const noexcept { Use(); }
+  void operator()() const noexcept { Use(); }
 
-   static std::string LoadSourceCode(std::string const& path) {
-     std::ifstream shader_file;
-     shader_file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-     std::string return_value = "";
-     try {
-       shader_file.open(path);
-       std::stringstream shaderStream;
-       shaderStream << shader_file.rdbuf();
-       shader_file.close();
-       return_value = shaderStream.str();
-     } catch (std::ifstream::failure const&) {
-       return_value = "";
-     }
-     return return_value;
-   }
+  static std::string LoadSourceCode(std::string const& path) {
+    std::ifstream shader_file;
+    shader_file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    std::string return_value = "";
+    try {
+      shader_file.open(path);
+      std::stringstream shaderStream;
+      shaderStream << shader_file.rdbuf();
+      shader_file.close();
+      return_value = shaderStream.str();
+    } catch (std::ifstream::failure const&) {
+      return_value = "";
+    }
+    return return_value;
+  }
 
-    void Use() const noexcept { glUseProgram(sp_id_); }
+  void Use() const noexcept { glUseProgram(sp_id_); }
 
-    void SetBool(const std::string& name, bool value) const;
-    void SetInt(const std::string& name, int value) const;
-    void SetUInt(const std::string& name, unsigned int value) const;
-    void SetFloat(const std::string& name, float value) const;
+  void SetBool(const std::string& name, bool value) const;
+  void SetInt(const std::string& name, int value) const;
+  void SetUInt(const std::string& name, unsigned int value) const;
+  void SetFloat(const std::string& name, float value) const;
 
-    void SetVec1(const std::string& name, const glm::vec1& value) const;
-    void SetVec2(const std::string& name, const glm::vec2& value) const;
-    void SetVec3(const std::string& name, const glm::vec3& value) const;
-    void SetVec4(const std::string& name, const glm::vec4& value) const;
+  void SetVec1(const std::string& name, const glm::vec1& value) const;
+  void SetVec2(const std::string& name, const glm::vec2& value) const;
+  void SetVec3(const std::string& name, const glm::vec3& value) const;
+  void SetVec4(const std::string& name, const glm::vec4& value) const;
 
-    void SetMat2(const std::string& name, const glm::mat2& value) const;
-    void SetMat3(const std::string& name, const glm::mat3& value) const;
-    void SetMat4(const std::string& name, const glm::mat4& value) const;
+  void SetMat2(const std::string& name, const glm::mat2& value) const;
+  void SetMat3(const std::string& name, const glm::mat3& value) const;
+  void SetMat4(const std::string& name, const glm::mat4& value) const;
 
-    void SetMat2x2(const std::string& name, const glm::mat2x2& value) const;
-    void SetMat2x3(const std::string& name, const glm::mat2x3& value) const;
-    void SetMat2x4(const std::string& name, const glm::mat2x4& value) const;
+  void SetMat2x2(const std::string& name, const glm::mat2x2& value) const;
+  void SetMat2x3(const std::string& name, const glm::mat2x3& value) const;
+  void SetMat2x4(const std::string& name, const glm::mat2x4& value) const;
 
-    void SetMat3x2(const std::string& name, const glm::mat3x2& value) const;
-    void SetMat3x3(const std::string& name, const glm::mat3x3& value) const;
-    void SetMat3x4(const std::string& name, const glm::mat3x4& value) const;
+  void SetMat3x2(const std::string& name, const glm::mat3x2& value) const;
+  void SetMat3x3(const std::string& name, const glm::mat3x3& value) const;
+  void SetMat3x4(const std::string& name, const glm::mat3x4& value) const;
 
-    void SetMat4x2(const std::string& name, const glm::mat4x2& value) const;
-    void SetMat4x3(const std::string& name, const glm::mat4x3& value) const;
-    void SetMat4x4(const std::string& name, const glm::mat4x4& value) const;
+  void SetMat4x2(const std::string& name, const glm::mat4x2& value) const;
+  void SetMat4x3(const std::string& name, const glm::mat4x3& value) const;
+  void SetMat4x4(const std::string& name, const glm::mat4x4& value) const;
 
-   private:
-    static int32_t Shader::CompileShader(std::string_view shader_code,
-                                         uint32_t& id, GLenum type);
+ private:
+  static int32_t Shader::CompileShader(std::string_view shader_code,
+                                       uint32_t& id, GLenum type);
 
-    // shader program id
-    unsigned int sp_id_ = 0;
+  // shader program id
+  unsigned int sp_id_ = 0;
 };
 }  // namespace engine::client::render
+
+namespace std {
+template <>
+struct hash<engine::client::render::Shader::ShaderSource> {
+  size_t operator()(
+      const engine::client::render::Shader::ShaderSource& k) const {
+    return ((hash<string_view>()(k.vertex_shader_code) ^
+             (hash<string_view>()(k.fragment_shader_code) << 1)) >>
+            1) ^
+           (hash<string_view>()(k.geometry_shader_code) << 1);
+  }
+};
+
+}  // namespace std
