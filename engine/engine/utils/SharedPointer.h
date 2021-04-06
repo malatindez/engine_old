dbc0b46138232ecdec1b5f3a38c30ccf2d12951c
malatindez <axenance@gmail.com>
2021-04-06 20:00:26 +0300

    Update SharedPointer.h

--- engine/engine/utils/SharedPointer.h
+++ engine/engine/utils/SharedPointer.h
@@ -2,6 +2,10 @@
 #include <memory>
 namespace engine::utils {
 // Shared pointer with mutex which can be used between multiple threads.
+// To write changes to the object you should call unique_lock on mutex_
+// To read the object data, you should call shared_lock on mutex_
+// After locking the object you should call Expired() method to ensure that the
+// object is still valid. After that you can continue the work with the object.
 template <class T>
 class SharedPointer : public std::shared_ptr<T> final {
  public:
